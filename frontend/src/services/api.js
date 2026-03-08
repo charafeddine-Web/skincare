7d7dfa8d81d2cc51987942e4c481534b79c042d7
charafeddine-Web <charafeddinetbibzat@gmail.com>
2026-03-08 21:32:07 +0000

    fix: corigier URL pour lie Front with backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configuration de base de l'API
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://evelinecosmetics.ma/';
 
 // Création de l'instance axios
 const api = axios.create({
